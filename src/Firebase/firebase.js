10acf19e23dc38cc1ac76dfa0cd78088f8fcc8d1
melisademirparmak <melisademirparmaak@gmail.com>
2021-12-17 18:48:11 +0300

    add: firebase

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVlmqG0gD4rHxkCW1VoBBLCJDii7Q9bkk',
+  authDomain: 'instagraam--clone.firebaseapp.com',
+  projectId: 'instagraam--clone',
+  storageBucket: 'instagraam--clone.appspot.com',
+  messagingSenderId: '416290852575',
+  appId: '1:416290852575:web:983201a60b6808292d236a',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+
+export default firebase;
