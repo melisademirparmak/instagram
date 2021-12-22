4cd3ec07d3c686b1a31b545d11aff280994acdf1
melisademirparmak <melisademirparmaak@gmail.com>
2021-12-22 09:49:38 +0300

    add: post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,9 +3,9 @@ import { db } from '../../firebase/firebase';
 import { FiMoreHorizontal } from 'react-icons/fi';
 import '../Post/post.css';
 import { BiMessageRounded } from 'react-icons/bi';
-import { RiCompassDiscoverLine } from 'react-icons/ri';
+import { BiNavigation } from 'react-icons/bi';
 import { FaRegHeart } from 'react-icons/fa';
-
+import { BiBookmark } from 'react-icons/bi';
 const Post = () => {
   const [post, setPost] = useState([]);
 
@@ -19,20 +19,32 @@ const Post = () => {
     <>
       {post.map((posts) => (
         <div className="post">
-          <div className="post__name">
-            {posts.name}
+          <div className="post__nav">
+            <div className="post__navDetail">
+              <img className="post__navImage" src={posts.user} alt="/" />
+              <h3>{posts.name}</h3>
+            </div>
             <FiMoreHorizontal />{' '}
           </div>
+
           <div className="post__imageCenter">
             <img src={posts.imageUrl} className="post__image" alt="/" />
           </div>
           <div className="post__icon">
-            <FaRegHeart size={23} />
-            <BiMessageRounded size={23} />
-            <RiCompassDiscoverLine size={23} />
+            <div className="post__iconRight">
+              <FaRegHeart size={22} />
+              <BiMessageRounded size={22} />
+              <BiNavigation size={20} />
+            </div>
+            <div className="post__iconLeft">
+              <BiBookmark size={22} />
+            </div>
           </div>
 
-          <div className="post__comment">{posts.comment}</div>
+          <div className="post__comment">
+            <h3>{posts.name}</h3>
+            {posts.comment}
+          </div>
         </div>
       ))}
     </>
